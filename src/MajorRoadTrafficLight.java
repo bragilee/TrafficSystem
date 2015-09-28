13e3ca973daac5bfbdd78ee3c3b1a0020e46f319
bragi <melbournelee17@gmail.com>
2015-09-28 20:21:29 +1000

    Major fixed.

--- src/MajorRoadTrafficLight.java
+++ src/MajorRoadTrafficLight.java
@@ -5,10 +5,41 @@ public class MajorRoadTrafficLight extends TrafficLight {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	void signalChange() {
-		// TODO Auto-generated method stub
-		
-	}
+	//one tick of the clock; move the lights through the pattern if necessary
+	  public void tick()
+	  {
+	    if (timer_ > TIMEOUT) {
+	      timer_--;
+	    }
 
-}
+	    super.tick();
+	  }
+
+	  //change the signal due to a sensor being triggered
+	  public void signalChange()
+	  {
+	    //if we are already iterating through a light change, then ignore
+	    //the signal.
+	    if (colour_ == LightColour.GREEN) {
+	      timer_ = TIMEOUT;
+	    }
+	  }
+
+	  protected void goGreen()
+	  {
+	    super.goGreen();
+	    timer_ = -1;
+	  }
+
+	  protected void goAmber()
+	  {
+	    super.goAmber();
+	    timer_ = AMBER_TIME;
+	  }
+
+	  protected void goRed()
+	  {
+	    super.goRed();
+	    timer_ = RED_TIME;
+	  }
+	}
